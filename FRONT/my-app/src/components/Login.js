107b09465af4f3a279008f4fae66c82b96a1dbda
rodrigo <rodrigo_betan@hotmail.com>
2020-05-16 02:29:38 -0500

    login básico

---
+++ FRONT/my-app/src/components/Login.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+let logo = require ("../images/logo.png")
+
+
+
+class Login extends Component {
+
+  
+
+    render () {
+        return (
+        <div 
+            className= "uk-position-center uk-vertical-align uk-text-center"
+            style={  {
+                width: "500px",
+                border: "solid black",
+                backgroundColor:  "#ADD8E6",
+                    }}   > 
+
+            <div>
+                <img width="200" 
+                     src={logo}
+                     alt="company"   />
+
+            <form>
+                    <div className="uk-form-row ">   
+
+                        <input  
+                            className="uk-width-1-1 uk-form-large uk-text-center"
+                            type="text"
+                            placeholder = "Username"        >
+                        </input>
+
+                        <input  
+                            className="uk-width-1-1 uk-form-large uk-text-center"
+                            type="text"
+                            placeholder = "Password"        >
+                        </input>
+
+                        <div className="uk-form-row ">         
+                            <button 
+                            className="uk-button uk-button-large"
+                            style={{ backgroundColor:  "#ADD8E6"}}>
+                            Login
+                            </button>
+                        
+                        </div>
+                    </div>
+            </form>
+
+        </div>
+        </div>
+    )}
+}
+
+export default Login
--- FRONT/my-app/src/index.js
+++ FRONT/my-app/src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import UIkit from "uikit"
+import "uikit/dist/css/uikit.min.css"
+import Icons from "uikit/dist/js/uikit-icons"
+
+UIkit.use(Icons)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />, 
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
