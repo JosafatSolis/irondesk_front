da33ca1941a4ac3759f455bb75e5634ffd327c20
rodrigo <rodrigo_betan@hotmail.com>
2020-05-19 23:09:11 -0500

    revisado

--- FRONT/my-app/src/components/ErrorDialog.js
+++ FRONT/my-app/src/components/ErrorDialog.js
@@ -1,13 +1,13 @@
 import React from "react"
 
-const Error = ({touched, message}) =>{
+const ErrorDialog = ({touched, message}) =>{
 
     if (!touched) {return <div className="uk-form-danger"> &nbsp; </div>}
     if (message) {return <div className="uk-form-danger"> {message} </div>}
 
-    return <div className= "uk-form-success"> continua...
+    return <div className= "uk-form-success"> pass...
 
     </div>
 }
 
-export default Error
+export default ErrorDialog
