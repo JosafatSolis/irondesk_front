9a6622d65c0d5c12c48c694f281df02f11497a31
rodrigo <rodrigo_betan@hotmail.com>
2020-05-22 00:55:38 -0500

    propuesta cards a detalle

--- FRONT/my-app/src/components/TicketCard.js
+++ FRONT/my-app/src/components/TicketCard.js
@@ -4,29 +4,85 @@ import DeviceCard from './DeviceCard';
 export default class TicketCard extends Component {
     render() {
         return (
-            <div className="uk-card uk-card-default">
-                <div className="uk-card-title">
-                    10:32:05
-                </div>
-                <div className="uk-card-body">
-                    <div>
-                        Tenant: BT - Bimbo Toluca
-                    </div>
-                    <div>
-                        Reporta:
-                        Juan Perez
-                    </div>
-                    <div>
-                        Descripción:
-                    </div>
-                    <div>
-                        Dejó de Funcionar el Equipo después de haber reiniciado por falla en el suministro eléctrico.
-                    </div>
-                    <div>
-                        Asignado a:
-                        David
-                    </div>
-                </div>
+            <div 
+            className="uk-container"
+            style={
+                {
+                 margin: "15px",
+                 width: "100%"
+            }
+            
+            }>
+               
+               
+
+    <table 
+    class="uk-table uk-table-hover uk-table-divider"
+   
+    >
+   
+    <tbody>
+
+        <tr>
+            <td>HORA DE APERTURA   </td>
+            <td>10:22             </td>
+        </tr>
+
+        <tr>
+            <td>HORA DE CIERRE   </td>
+            <td>    PENDIENTE       </td>
+        </tr>
+
+        <tr>
+            <td>TENANT          </td>
+            <td>BT              </td>
+        </tr>
+
+        <tr>
+            <td>REPORTA         </td>
+            <td>Juan Perez      </td>
+        </tr>
+        
+        <tr>
+            <td>ASIGNADO A       </td>
+            <td> El deivid     </td>
+        </tr>
+
+        <tr>
+            <td>DESCRIPCIÓN        </td>
+            <td>
+                Dejo de funcionar el equipo BLABLABALABLABALABLABALBALABALAKSJDKLSAJDASHDIUFGIAUFBCAICBAIKCBAKJCBAICJBAIKCBAIBLDNCLJDCHNLJDKCHNDJLSKC     
+            </td>
+        </tr>
+
+
+        <tr>
+            <td>ACTIVIDADES        </td>
+            <td 
+         
+            className="uk-column-1-1 container-fluid"
+            >
+                <div>ACTIVIDAD 1 </div>
+                <div>ACTIVIDAD 2 </div>
+                <div>ACTIVIDAD 3 </div>
+                <div>ACTIVIDAD 4 </div>
+                
+                   
+
+
+            </td>
+        </tr>
+
+    </tbody>
+</table>
+    
+
+
+
+
+
+              
+              
             </div>
         )
     }
--- FRONT/my-app/src/components/TicketsContainer.js
+++ FRONT/my-app/src/components/TicketsContainer.js
@@ -10,7 +10,7 @@ export default class TicketsContainer extends Component {
                     <div className="uk-grid uk-child-width-1-3">
                         <TicketCard code="BT" name="Bimbo Toluca" />
                         <TicketCard code="TR" name="Tia Rosa" />
-                        <TicketCard code="SM" name="Suandy" />
+                       
                     </div>
                 
                 </div>
