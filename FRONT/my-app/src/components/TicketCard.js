4f6276fa832c7be37b258d4de7648f82ba5dfb8b
Josafat Solis <jsolis@macroaplicaciones.com>
2020-05-22 22:53:10 -0500

    Pruebas con Filter

--- FRONT/my-app/src/components/TicketCard.js
+++ FRONT/my-app/src/components/TicketCard.js
@@ -4,77 +4,93 @@ import DeviceCard from './DeviceCard';
 export default class TicketCard extends Component {
     render() {
         return (
-            <div 
-            className="uk-container"
-            style={
-                {
-                 margin: "15px",
-                 width: "100%"
-            }
-            
-            }>
+            <div className="uk-card uk-card-default">
+                <div class="uk-card-badge uk-label">Abierto</div>
+                <div className="uk-card-title">
+                    10:32:05
+                </div>
+                <div className="uk-card-body">
+                    <div>
+                        Tenant: BT - Bimbo Tolucas
+                    </div>
+                    <div>
+                        Reporta:
+                        Juan Perez
+                    </div>
+                    <div>
+                        Descripción:
+                    </div>
+                    <div>
+                        Dejó de Funcionar el Equipo después de haber reiniciado por falla en el suministro eléctrico.
+                    </div>
+                    <div>
+                        Asignado a:
+                        David
+                    </div>
+                </div>
+            </div>
+        )   
                
-               
 
-    <table 
-    class="uk-table uk-table-hover uk-table-divider"
+//     <table 
+//     class="uk-table uk-table-hover uk-table-divider"
    
-    >
+//     >
    
-    <tbody>
+//     <tbody>
 
-        <tr>
-            <td>HORA DE APERTURA   </td>
-            <td>10:22             </td>
-        </tr>
+//         <tr>
+//             <td>HORA DE APERTURA   </td>
+//             <td>10:22             </td>
+//         </tr>
 
-        <tr>
-            <td>HORA DE CIERRE   </td>
-            <td>    PENDIENTE       </td>
-        </tr>
+//         <tr>
+//             <td>HORA DE CIERRE   </td>
+//             <td>    PENDIENTE       </td>
+//         </tr>
 
-        <tr>
-            <td>TENANT          </td>
-            <td>BT              </td>
-        </tr>
+//         <tr>
+//             <td>TENANT          </td>
+//             <td>BT              </td>
+//         </tr>
 
-        <tr>
-            <td>REPORTA         </td>
-            <td>Juan Perez      </td>
-        </tr>
+//         <tr>
+//             <td>REPORTA         </td>
+//             <td>Juan Perez      </td>
+//         </tr>
         
-        <tr>
-            <td>ASIGNADO A       </td>
-            <td> El deivid     </td>
-        </tr>
+//         <tr>
+//             <td>ASIGNADO A       </td>
+//             <td> El deivid     </td>
+//         </tr>
 
-        <tr>
-            <td>DESCRIPCIÓN        </td>
-            <td>
-                Dejo de funcionar el equipo BLABLABALABLABALABLABALBALABALAKSJDKLSAJDASHDIUFGIAUFBCAICBAIKCBAKJCBAICJBAIKCBAIBLDNCLJDCHNLJDKCHNDJLSKC     
-            </td>
-        </tr>
+//         <tr>
+//             <td>DESCRIPCIÓN        </td>
+//             <td>
+//                 Dejo de funcionar el equipo BLABLABALABLABALABLABALBALABALAKSJDKLSAJDASHDIUFGIAUFBCAICBAIKCBAKJCBAICJBAIKCBAIBLDNCLJDCHNLJDKCHNDJLSKC     
+//             </td>
+//         </tr>
 
 
-        <tr>
-            <td>ACTIVIDADES        </td>
-            <td 
+//         <tr>
+//             <td>ACTIVIDADES        </td>
+//             <td 
          
-            className="uk-column-1-1 container-fluid"
-            >
-                <div>ACTIVIDAD 1 </div>
-                <div>ACTIVIDAD 2 </div>
-                <div>ACTIVIDAD 3 </div>
-                <div>ACTIVIDAD 4 </div>
+//             className="uk-column-1-1 container-fluid"
+//             >
+//                 <div>ACTIVIDAD 1 </div>
+//                 <div>ACTIVIDAD 2 </div>
+//                 <div>ACTIVIDAD 3 </div>
+//                 <div>ACTIVIDAD 4 </div>
                 
                    
 
 
-            </td>
-        </tr>
+//             </td>
+//         </tr>
 
-    </tbody>
-</table>
+//     </tbody>
+// </table>
     
 
 
@@ -83,7 +99,7 @@ export default class TicketCard extends Component {
 
               
               
-            </div>
-        )
+//             </div>
+//         )
     }
 }
--- FRONT/my-app/src/components/TicketsContainer.js
+++ FRONT/my-app/src/components/TicketsContainer.js
@@ -1,20 +1,75 @@
-import React, { Component } from 'react';
-import TicketCard from './TicketCard';
-
+import React, { Component } from "react";
+import TicketCard from "./TicketCard";
 
 export default class TicketsContainer extends Component {
-    render() {
-        return(
-            <section className="uk-section">
-                <div className="uk-container">
+  render() {
+    return (
+      <section className="uk-section">
+        {/* <div className="uk-container">
                     <div className="uk-grid uk-child-width-1-3">
                         <TicketCard code="BT" name="Bimbo Toluca" />
                         <TicketCard code="TR" name="Tia Rosa" />
                        
                     </div>
                 
-                </div>
-            </section>
-        )
-    }
+                </div> */}
+
+        <div uk-filter="target: .js-filter">
+          <ul className="uk-subnav uk-subnav-pill">
+            <li className="uk-active" uk-filter-control="">
+              <a href="#">Todos</a>
+            </li>
+            <li uk-filter-control="[status='Abierto']">
+              <a href="#">Abiertos</a>
+            </li>
+            <li uk-filter-control="[status='Cerrado']">
+              <a href="#">Cerrados</a>
+            </li>
+          </ul>
+
+          <ul
+            className="js-filter uk-child-width-1-2 uk-child-width-1-3@m uk-text-center"
+            uk-grid
+          >
+            <li status="Abierto">
+            <TicketCard code="BT" name="Bimbo Toluca" />
+            </li>
+            <li status="Abierto">
+            <TicketCard code="BT" name="Bimbo Toluca" />
+            </li>
+            <li status="Abierto">
+            <TicketCard code="BT" name="Bimbo Toluca" />
+            </li>
+            <li status="Abierto">
+            <TicketCard code="BT" name="Bimbo Toluca" />
+            </li>
+            <li status="Cerrado">
+            <TicketCard code="BT" name="Bimbo Toluca" />
+            </li>
+            <li status="Cerrado">
+            <TicketCard code="BT" name="Bimbo Toluca" />
+            </li>
+            <li status="Abierto">
+            <TicketCard code="BT" name="Bimbo Toluca" />
+            </li>
+            <li status="Cerrado">
+            <TicketCard code="BT" name="Bimbo Toluca" />
+            </li>
+            <li status="Abierto">
+              <div className="uk-card uk-card-primary uk-card-body">Item</div>
+            </li>
+            <li status="Abierto">
+              <div className="uk-card uk-card-default uk-card-body">Item</div>
+            </li>
+            <li status="Abierto">
+              <div className="uk-card uk-card-primary uk-card-body">Item</div>
+            </li>
+            <li status="Cerrado">
+              <div className="uk-card uk-card-secondary uk-card-body">Item</div>
+            </li>
+          </ul>
+        </div>
+      </section>
+    );
+  }
 }
