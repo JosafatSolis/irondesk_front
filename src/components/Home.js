6a80b55c409a81ccf05cef304048b8f4a0a3bf46
Josafat Solis <jsolis@macroaplicaciones.com>
2020-05-28 14:54:27 -0500

    Avance con barra de nav

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 
 import FormikForm from "./FormikForm";
 import NavBar from "./Navbar";
@@ -10,32 +10,79 @@ import UsersContainer from "./UsersContainer";
 import AppContext from "../AppContext";
 
 export default class Home extends Component {
-static contextType = AppContext;
+  static contextType = AppContext;
 
-render() {
-    const users =[
-      {id:"abc", email:"mario@mapps.com", role:"User", name: "Mario", lastName:"Terrazas", tenantCode:"TR", phone:"55 10 40 50 68" },
-      {id:"bcd", email:"josa@mapps.com", role:"Admin", name: "Josafat", lastName:"Solis", tenantCode:"MAPPS", phone:"55 10 40 00 99" },
-      {id:"bdc", email:"rod@mapps.com", role:"Admin", name: "Rodrigo", lastName:"Amador", tenantCode:"HZ", phone:"55 88 40 00 99" },
+    
+
+  componentDidMount() {
+    const { history } = this.props;
+    const { _id, role, tenant } = this.context.currentUser;
+    if (!_id) {
+      history.push("/login");
+    } else {
+      if (["Admin", "Tecnician"].includes(role)) {
+        history.push("/home/tenants");
+      } else {
+        history.push(`/home/tickets/${tenant._id}`);
+      }
+    }
+  }
+
+  render() {
+    const users = [
+      {
+        id: "abc",
+        email: "mario@mapps.com",
+        role: "User",
+        name: "Mario",
+        lastName: "Terrazas",
+        tenantCode: "TR",
+        phone: "55 10 40 50 68",
+      },
+      {
+        id: "bcd",
+        email: "josa@mapps.com",
+        role: "Admin",
+        name: "Josafat",
+        lastName: "Solis",
+        tenantCode: "MAPPS",
+        phone: "55 10 40 00 99",
+      },
+      {
+        id: "bdc",
+        email: "rod@mapps.com",
+        role: "Admin",
+        name: "Rodrigo",
+        lastName: "Amador",
+        tenantCode: "HZ",
+        phone: "55 88 40 00 99",
+      },
     ];
     return (
       // Manda los valores al AppContext para que estén disponibles en todos lados
-        <div className="App">    
-          <NavBar />
-          {/* <UsersContainer users={users} /> */}
-          <Switch>
-            <Route exact path="/home/tenants" component={TenantsContainer} /> 
-            <Route exact path="/home/users" component={(props) => <UsersContainer  {...props} users={users}/>} />
-            {/* 
+      <div className="App">
+        <NavBar />
+        {/* <UsersContainer users={users} /> */}
+        <Switch>
+          <Route exact path="/home/tenants" component={TenantsContainer} />
+          <Route
+            exact
+            path="/home/users"
+            component={(props) => <UsersContainer {...props} users={users} />}
+          />
+          {/* 
                 // ¡¡Cuidado con hacer esto!! Porque hace que no lleguen los props al componente
                 <Route exact path="/home/tickets/:tenantId" component={() => TicketsContainer} />
             */}
-            <Route exact path="/home/tickets" component={TicketsContainer} />
-            <Route exact path="/home/tickets/:tenantId" component={TicketsContainer} />
-            <Route exact path="/home/tickets/new" component={FormikForm} />
-          </Switch>
-        </div>  
+          <Route exact path="/home/tickets" component={TicketsContainer} />
+          <Route
+            exact
+            path="/home/tickets/:tenantId"
+            component={TicketsContainer}
+          />
+          <Route exact path="/home/tickets/new" component={FormikForm} />
+        </Switch>
+      </div>
     );
   }
-
 }
--- src/components/Modals_Forms/ModalButtonTenant.js
+++ src/components/Modals_Forms/ModalButtonTenant.js
@@ -1,55 +1,59 @@
-import React, {useState} from "react";
-import Modal from "react-modal"
+import React, { useState } from "react";
+import Modal from "react-modal";
 import FormikFormTenant from "./FormikFormTenant";
 
 const customStyles = {
   content: {
-    top: '25%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
+    top: "25%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
     width: "30%",
-    height: '45%',
-    transform: 'translate(-40%, -20%)',
-    border: 'solid 1px #f07e1c'
-  
+    height: "45%",
+    transform: "translate(-40%, -20%)",
+    border: "solid 1px #f07e1c",
   },
 };
 
-function ModalButton () {
-    const [modalIsOpen, setModalIsOpen] = useState(false)
-  
-    return (
-      <div className=" uk-text-center">
+function ModalButton() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
-        <button 
-          onClick= { ()=>{setModalIsOpen(true)}  }
-          className="uk-button uk-button-primary"> 
-            New "COMPONENT"
-        </button>
+  return (
+    <div className=" uk-text-center">
+      <button
+        onClick={() => {
+          setModalIsOpen(true);
+        }}
+        className="uk-button uk-button-primary"
+      >
+        Nuevo Tenant [+]
+      </button>
 
-          <Modal 
-            isOpen={modalIsOpen}
-            onRequestClose={()=>{setModalIsOpen(false)}}
-            style={customStyles}>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => {
+          setModalIsOpen(false);
+        }}
+        style={customStyles}
+      >
+        <div>
+          <a
+            href="#"
+            onClick={() => {
+              setModalIsOpen(false);
+            }}
+            className="uk-button uk-button-muted uk-align-right"
+          >
+            <span uk-icon="icon: close"> </span>
+          </a>{" "}
+          <br />
+        </div>
 
-          <div>
-            <a 
-              href="#"
-              onClick= {()=>{setModalIsOpen(false)}  }
-              className="uk-button uk-button-muted uk-align-right">
-              <span uk-icon="icon: close"> </span>     
-            </a> <br/>
-            </div>
-            
-            <br/>
-            <FormikFormTenant/>  
-                 
-            
-
-          </Modal>
-      </div>
-    );
-  }
+        <br />
+        <FormikFormTenant />
+      </Modal>
+    </div>
+  );
+}
 
 export default ModalButton;
